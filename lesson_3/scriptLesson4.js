36a1af337447af8d4c3433ec12d762500eabf2bb
Oleksiy <89708793+AleksiyDem@users.noreply.github.com>
2023-06-20 16:27:09 +0300

    1.3

---
+++ lesson_3/scriptLesson4.js
@@ -0,0 +1,17 @@
+
+//  lesson 4 ++++++++++++++++++++++++++++++++++=======================================
+
+// let - зміни создаємо в першу частину!  потім вже вірешення
+// prompt - дає стрінгу, а щоб number перед parseInt !!!! 
+
+// to instal prettier !!! 
+
+//  < after > then === dont use becouse its not necessary!!!  else can use
+//` ` use only with insert like this ${} 
+
+
+
+
+const A = 23;
+A =27;
+console.log(A);
